e509e79fc8408fe6ff2a96b27e61d5a24161c298
TimmyChan99 <felemenoun@gmail.com>
2026-05-07 07:53:57 +0100

    Add pages

--- project/polls/admin.py
+++ project/polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+admin.site.site_header = "Polls Admin"
+admin.site.site_title = "Polls Admin Portal"
+admin.site.index_title = "Welcome to the Polls Admin Portal"
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
